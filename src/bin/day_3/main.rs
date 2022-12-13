96e427dd60c4ca3172b700f60ae19c0e7880a60d
Jonathan BAUDIN <jjbaudin@gmail.com>
2022-12-13 21:32:57 +0100

    Add part 2 of day 3

--- src/bin/day_3/main.rs
+++ src/bin/day_3/main.rs
@@ -1,4 +1,4 @@
-use std::fs;
+use std::{fs, vec};
 
 fn main() {
     println!("\n=== Day 3  ====");
@@ -8,32 +8,51 @@ fn main() {
         fs::read_to_string("src/bin/day_3/input.txt").expect("Unable to find 'input.txt' !");
     //let inputs = "vJrwpWtwJgWrhcsFMMfFFhFp\njqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL\nPmmdzqPrVvPwwTWBwg\nwMqvLMZHhHMvwLHjbvcjnnSBnvTQFn\nttgJtRGJQctTZtZT\nCrZsJsPPZsGzwwsLwLmpwMDw\n\n".to_string();
 
-    let mut total: i32 = 0;
+    let mut total_priority: i32 = 0;
+    let mut total_badges: i32 = 0;
+    let mut pack: Vec<&str> = Vec::new();
 
     for x in inputs.split('\n') {
         if x.is_empty() {
             continue;
         }
 
+        /* ===== FIRST PART ===== */
+
         let (first, second) = x.split_at(x.len() / 2);
         let common = find_common(first, second);
 
+        total_priority += letter_to_priority(common) as i32;
 
-        total += letter_to_priority(common) as i32;
+
+        /* ===== SECOND PART ===== */
+
+        pack.push(x);
+        if pack.len() >= 3 {
+            total_badges += letter_to_priority( find_badge(&pack) ) as i32;
+            pack.clear();
+        }
     }
 
-    println!("\nPart one answer: {}", total);
-    // println!("\nPart two answer: {}", score_part_2);
+    println!("\nPart one answer: {}", total_priority);
+    println!("\nPart two answer: {}", total_badges);
 }
 
+fn find_badge(pack: &Vec<&str>) -> char{
+    for c in pack.get(0).unwrap().chars(){
+        if pack.get(1).unwrap().contains(c) && pack.get(2).unwrap().contains(c) {
+            return c;
+        }
+    }
+
+    return '\0';
+}
 
 fn find_common(a: &str, b: &str) -> char{
 
     for x in a.chars(){
-        for y in b.chars() {
-            if x == y {
-                return x;
-            }
+        if b.contains(x) {
+            return x;
         }
     }
 
