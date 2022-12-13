dc7422d491f37f8e21515efd2816f71e676f348c
Jonathan BAUDIN <jjbaudin@gmail.com>
2022-12-13 12:00:12 +0100

    Refactor for part 2

--- src/day_1/main.rs
+++ src/day_1/main.rs
@@ -4,22 +4,22 @@ fn main() {
     println!("\n=== Day 1  ====");
     println!("Opening intput.txt");
 
-    let mut current: i32 = 0;
-    let mut biggest : i32 = 0;
+    let mut biggests: [i32; 4] = [0, 0, 0, 0];
+
     let inputs = fs::read_to_string("src/day_1/input.txt").expect("Unable to find 'input.txt' !");
 
     for x in inputs.split('\n'){
         if x.is_empty(){
-            if current > biggest {
-                biggest = current;
-            }
+            biggests.sort();
+            biggests.reverse();
 
-            current = 0;
+            biggests[3] = 0;
         }
         else {
-            current += x.parse::<i32>().unwrap();
+            biggests[3] += x.parse::<i32>().unwrap();
         }
     }
 
-    println!("\nAnswer: {}", biggest);
+    println!("\nPart one answer: {}", biggests[0]);
+    println!("\nPart two answer: {}", biggests[0] + biggests[1] + biggests[2]);
 }
