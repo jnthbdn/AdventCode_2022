c86b3add625308fdfcd35a2b1ff59e4f66ff1f06
Jonathan BAUDIN <jjbaudin@gmail.com>
2022-12-14 17:41:28 +0100

    add second part day 6

--- src/bin/day_6/main.rs
+++ src/bin/day_6/main.rs
@@ -4,26 +4,39 @@ fn main() {
     println!("\n=== Day 6  ====");
 
     let inputs =
-    fs::read_to_string("src/bin/day_6/input.txt").expect("Unable to find 'input.txt' !");
-    // "bvwbjplbgvbhsrlpgdmjqwftvncz\n".to_string();
+        fs::read_to_string("src/bin/day_6/input.txt").expect("Unable to find 'input.txt' !");
+    // "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw\n".to_string();
+
+    const SIZE_FIRST_MARKER: usize = 4;
+    const SIZE_SECOND_MARKER: usize = 14;
 
     let bytes = inputs.as_bytes();
     let mut first_marker: usize = 0;
+    let mut second_marker: usize = 0;
 
-    for i in 4..inputs.len(){
-        if ! contains_duplicate(&bytes[i-4..i]){
+    /* ===== FIRST PART ===== */
+    for i in SIZE_FIRST_MARKER..inputs.len() {
+        if !contains_duplicate(&bytes[i - SIZE_FIRST_MARKER..i]) {
             first_marker = i;
             break;
         }
     }
 
+    /* ===== SECOND PART ===== */
+    for i in SIZE_SECOND_MARKER..inputs.len() {
+        if !contains_duplicate(&bytes[i - SIZE_SECOND_MARKER..i]) {
+            second_marker = i;
+            break;
+        }
+    }
+
     println!("\nPart one answer: {}", first_marker);
-    // println!("\nPart two answer: {}", String::from_iter(stacks_2.iter().map(|x| x.back().unwrap())));
+    println!("\nPart two answer: {}", second_marker);
 }
 
-fn contains_duplicate(tab: &[u8]) -> bool{
-    for i in 0..(tab.len() - 1){
-        for j in (i+1)..tab.len(){
+fn contains_duplicate(tab: &[u8]) -> bool {
+    for i in 0..(tab.len() - 1) {
+        for j in (i + 1)..tab.len() {
             if tab[i] == tab[j] {
                 return true;
             }
