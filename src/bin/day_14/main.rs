d22632087b6873edd2b7ce9da4d936d1c32945cd
Jonathan BAUDIN <jjbaudin@gmail.com>
2023-01-02 19:15:41 +0100

    add day 14

---
+++ src/bin/day_14/main.rs
@@ -0,0 +1,12 @@
+use std::fs;
+
+fn main() {
+    println!("\n=== Day 14 ====");
+
+    let inputs =
+        fs::read_to_string("src/bin/day_14/input.txt").expect("Unable to find 'input.txt' !");
+        // fs::read_to_string("src/bin/day_13/example.txt").expect("Unable to find 'input.txt' !");
+
+    // println!("\nPart one answer: {}", sum_id_correct);
+    // println!("\nPart two answer: {}", first_divider_pos * second_divider_pos);
+}
